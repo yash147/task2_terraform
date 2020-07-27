11fbf551e7ae8a9b77230aa2d24629b17adfef91
Yash Barange <60816624+yash147@users.noreply.github.com>
2020-07-27 13:02:12 +0530

    Index page

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Terraform Cloud</title>
+  </head>
+<body>
+
+<h1>Amazon Web Service :- Cloud Computing Demo</h1>
+
+<pre>
+<?php
+
+$file = file_get_contents('url.txt');
+
+echo '<img src="'.$file.'"  width="500" height="600">';
+?>
+</pre>
+
+
+</body>
+</html>
